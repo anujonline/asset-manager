b514d5ec4033b2362e9f2b4965319591d17e89e9
start.vaadin.com <no-reply@vaadin.com>
2023-11-23 19:36:03 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,60 @@
+package com.example.application.views.myview;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import java.util.ArrayList;
+import java.util.List;
+
+@PageTitle("My View")
+@Route(value = "my-view", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        TextField textField = new TextField();
+        ComboBox comboBox = new ComboBox();
+        ComboBox comboBox2 = new ComboBox();
+        Button buttonPrimary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        textField.setLabel("Amount");
+        textField.setWidth("min-content");
+        comboBox.setLabel("Currency");
+        comboBox.setWidth("min-content");
+        setComboBoxSampleData(comboBox);
+        comboBox2.setLabel("Account Information");
+        comboBox2.setWidth("min-content");
+        setComboBoxSampleData(comboBox2);
+        buttonPrimary.setText("Save");
+        buttonPrimary.setWidth("min-content");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        getContent().add(textField);
+        getContent().add(comboBox);
+        getContent().add(comboBox2);
+        getContent().add(buttonPrimary);
+    }
+
+    record SampleItem(String value, String label, Boolean disabled) {
+    }
+
+    private void setComboBoxSampleData(ComboBox comboBox) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem("first", "First", null));
+        sampleItems.add(new SampleItem("second", "Second", null));
+        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
+        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        comboBox.setItems(sampleItems);
+        comboBox.setItemLabelGenerator(item -> ((SampleItem) item).label());
+    }
+}
